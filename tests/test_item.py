8e148a264c76a064865414a6abd53405d294a08b
Egor Savelyev <savelev0794@mail.ru>
2023-09-19 17:32:10 +0500

    fixed test/test_item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,15 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 
 
-def test_calculate_total_price():
-    """TestCase №1 общая стоимость товара в магазине"""
-    Item("помидор", 10000, 5)
-    assert 50000
+@pytest.fixture
+def item1():
+    """Создаем экземпляр класса в фикстуре"""
+    return Item("Смартфон", 10000, 20)
 
 
-def test_apply_discount():
-    """TestCase №2 применение скидки"""
-    Item.pay_tate = 0.8
-    Item("помидор", 10000, 5)
-    assert 8000
+def test_apply_discount(item1):
+    """TestCase №1 применение скидки"""
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
+
+
+def test_calculate_total_price(item1):
+    """TestCase №2 общая стоимость товара в магазине"""
+    assert item1.calculate_total_price() == 200000
