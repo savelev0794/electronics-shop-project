12cfa810611acffc7cc643ac239e243a4d8b351e
Egor Savelyev <savelev0794@mail.ru>
2023-09-26 22:30:53 +0500

    add new tests in test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,7 +7,7 @@ from src.item import Item
 @pytest.fixture
 def item1():
     """Создаем экземпляр класса в фикстуре"""
-    return Item("Смартфон", 10000, 20)
+    return Item("КрутойСмартфон", 10000, 20)
 
 
 def test_apply_discount(item1):
@@ -24,16 +24,27 @@ def test_calculate_total_price(item1):
 
 def test_name(item1):
     """TestCase №3 проверка имени"""
-    assert item1.name == 'Смартфон'
+    assert item1.name == 'КрутойСмартфон'
 
 
 def test_len_name():
-    """TestCase №4 максимальная длины имени"""
+    """TestCase №4 максимальная длина имени"""
     item1 = Item('ДлинныйСмартфон', 100, 20)
     assert len(item1.name) >= 10
 
 
+def test_len_cut(item1):
+    """TestCase №5 обрезка имени имени"""
+    item1.name = 'КрутойСмартфон'
+    assert item1.name == 'КрутойСмар'
+
+
 def test_string_to_number(item1):
-    """TestCase №5 возвращение числа из строки"""
+    """TestCase №6 возвращение числа из строки"""
     string_to_number = item1.string_to_number('5.4')
     assert string_to_number == 5
+
+
+# def test_instantiate_from_csv():
+#     """TestCase №7 создание объекта из файла csv"""
+
